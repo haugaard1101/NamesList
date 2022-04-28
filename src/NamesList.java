72ae8143e36b2c0393d06b57b142c74668a08d7e
Kasper <s205434@student.dtu.dk>
2022-04-28 13:33:23 +0200

    done maybe

--- src/NamesList.java
+++ src/NamesList.java
@@ -5,97 +5,103 @@ import java.util.Scanner;
 
 public class NamesList {
 
-    private ArrayList<String> names;
+  private ArrayList<String> names;
 
-    public NamesList() {
-        names = new ArrayList<>();
-    }
+  public NamesList() {
+    names = new ArrayList<>();
+  }
 
-    public void startUserInterface() throws FileNotFoundException {
-        System.out.println("""
-                Welcome to the NamesList - enterprise edition.
-                ----------------------------------------------
-                """);
+  public void startUserInterface() throws FileNotFoundException {
+    System.out.println("""
+        Welcome to the NamesList - enterprise edition.
+        ----------------------------------------------
+        """);
 
-        Scanner sc = new Scanner(System.in);
-        int choice = 99;
-        while( choice != 0) {
-            showMenu();
-            choice = sc.nextInt();
-            switch (choice) {
-                case 1 -> displayListOfNames();
-                case 2 -> loadListOfNames();
-                case 3 -> saveListOfNames();
-                case 4 -> enterNames();
-                case 0 -> exit();
-                default -> System.out.println("Unknown command - please use 0-4");
-            }
-
-        }
-    }
-
-    private void showMenu() {
-        System.out.println("""
-                1) Display list of names
-                2) Load list of names (not implemented)
-                3) Save list of names (not implemented)
-                4) Enter names
-                0) Exit
-                """);
-    }
-
-    private void enterNames() throws FileNotFoundException {
-        System.out.println("""
-                Enter names
-                -----------
-                Enter each name you want to add to the list. End by entering an empty name.
-                """);
-        Scanner sc = new Scanner(new File("names.txt"));
-        String name = "-nothing yet-";
-        while(!name.isBlank() && sc.hasNextLine()) {
-            name = sc.nextLine();
-            if(!name.isBlank()) {
-                names.add(name);
-                System.out.println(name + " added to the list, enter another, or empty to quit");
-            }
-        }
-        System.out.println("Done");
-    }
-
-    private void saveListOfNames() {
-        // TODO: Implement save of the names list to a file
-        System.out.println("NOT IMPLEMENTED");
-    }
-
-    private void loadListOfNames() {
-        // TODO: Implement load of the names list from a file
-        System.out.println("NOT IMPLEMENTED");
-    }
-
-    private void displayListOfNames() {
-        for(String name : names) {
-            System.out.println(name);
-        }
-        String isAre = "are";
-        String s = "s";
-        if(names.size() == 1) {
-            isAre = "is";
-            s = "";
-        }
-        System.out.println("There " + isAre + " " + names.size() + " name"+s+" in the system");
-    }
-
-    private void exit() {
-        System.out.println("""
-                ...
-                Thank you for using NamesList - enterprise edition.
-                Don't forget to subscribe to our newsletter!""");
+    Scanner sc = new Scanner(System.in);
+    int choice = 99;
+    while (choice != 0) {
+      showMenu();
+      choice = sc.nextInt();
+      switch (choice) {
+        case 1 -> displayListOfNames();
+        case 2 -> loadListOfNames();
+        case 3 -> saveListOfNames();
+        case 4 -> enterNames();
+        case 0 -> exit();
+        default -> System.out.println("Unknown command - please use 0-4");
+      }
 
     }
+  }
 
+  private void showMenu() {
+    System.out.println("""
+        1) Display list of names
+        2) Load list of names
+        3) Save list of names (not implemented)
+        4) Enter names
+        0) Exit
+        """);
+  }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        NamesList app = new NamesList();
-        app.startUserInterface();
+  private void enterNames() {
+    System.out.println("""
+        Enter names
+        -----------
+        Enter each name you want to add to the list. End by entering an empty name.
+        """);
+    Scanner sc = new Scanner(System.in);
+    String name = "-nothing yet-";
+    while (!name.isBlank() && sc.hasNextLine()) {
+      name = sc.nextLine();
+      if (!name.isBlank()) {
+        names.add(name);
+        System.out.println(name + " added to the list, enter another, or empty to quit");
+      }
     }
+    System.out.println("Done");
+  }
+
+  private void saveListOfNames() {
+    // TODO: Implement save of the names list to a file
+    System.out.println("NOT IMPLEMENTED");
+  }
+
+  private void loadListOfNames() throws FileNotFoundException {
+    Scanner loader = new Scanner(new File("names.txt"));
+    String name;
+    while (loader.hasNextLine()) {
+      name = loader.nextLine();
+      names.add(name);
+      System.out.println(name + " - added");
+    }
+    System.out.println("Done");
+  }
+
+  private void displayListOfNames() {
+    for (String name : names) {
+      System.out.println(name);
+    }
+    String isAre = "are";
+    String s = "s";
+    if (names.size() == 1) {
+      isAre = "is";
+      s = "";
+    }
+    System.out.println("There " + isAre + " " + names.size() + " name" + s + " in the system");
+  }
+
+  private void exit() {
+    System.out.println("""
+        ...
+        Thank you for using NamesList - enterprise edition.
+        Don't forget to subscribe to our newsletter!""");
+
+  }
+
+
+  public static void main(String[] args) throws FileNotFoundException {
+    NamesList app = new NamesList();
+    app.startUserInterface();
+  }
 }
